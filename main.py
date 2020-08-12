41f9b3d4908eec98745e4a2fedbd627ddc44c403
Charles Shi <66085065+CharlesShi12@users.noreply.github.com>
2020-08-12 14:41:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,17 +20,13 @@ if uploaded_file is not None:
     st.write("Classifying...")
 
     image = image.resize((150, 150))
-    #img = keras.preprocessing.image.load_img(image, target_size=(150, 150))
     img_tensor = keras.preprocessing.image.img_to_array(image)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)         
     img_tensor /= 255.
-    # x = np.true_divide(x, 255)
 
     pred = model.predict(img_tensor)
 
     if pred>0.9:
         st.write('Your image has been classified. There is no visible tumor in your MRI scan.')
-        st.write('Accuracy: ')
-        st.write(pred[0][0]*100)
     if pred<0.05:
         st.write('Your image has been classified. Your MRI scan contains a tumor.')
